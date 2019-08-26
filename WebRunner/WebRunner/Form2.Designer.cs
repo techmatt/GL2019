a1a8d4776453b8338778db20eae64940a25d16f6
Matt Fisher <techmatt@gmail.com>
2019-08-26 13:05:05 -0700

    .

--- WebRunner/WebRunner/Form2.Designer.cs
+++ WebRunner/WebRunner/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WebRunner
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -29,44 +29,44 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(47, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1031, 826);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1033, 49);
+            this.button1.Location = new System.Drawing.Point(1114, 76);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 65);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(213, 63);
+            this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(960, 703);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // Form1
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1255, 781);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1700, 983);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -74,9 +74,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
     }
 }
-
--- WebRunner/WebRunner/Form2.cs
+++ WebRunner/WebRunner/Form2.cs
@@ -1,7 +1,7 @@
-﻿
-using Emgu.CV;
+﻿using Emgu.CV;
 using Emgu.CV.Aruco;
 using Emgu.CV.Util;
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,31 +10,26 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-/*
- * https://github.com/emgucv/emgucv/blob/master/Emgu.CV.Example/Aruco/MainForm.cs
- * https://docs.opencv.org/3.2.0/d9/d6a/group__aruco.html#ga13a2742381c0a48e146d230a8cda2e66
- * http://www.emgu.com/wiki/files/3.2.0/document/html/763bec7d-ea86-f394-1e61-2dbbf59a0d6f.htm
- * https://docs.opencv.org/3.1.0/d5/dae/tutorial_aruco_detection.html
- */
 namespace WebRunner
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        VideoCapture capture = new VideoCapture(1); //create a camera capture
+        VideoCapture capture = new VideoCapture(0); //create a camera capture
         Dictionary arucoDictionary = new Dictionary(Dictionary.PredefinedDictionaryName.Dict4X4_50);
         Mat frameCopy = new Mat();
         DetectorParameters detectorParameters = DetectorParameters.GetDefault();
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void runDetection(Mat frame)
