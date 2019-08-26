3686c3c5c67b07ecaafe3873ff28271c5966d9a1
Matt Fisher <techmatt@gmail.com>
2019-08-25 22:35:01 -0700

    .

---
+++ WebRunner/WebRunner/Form1.cs
@@ -0,0 +1,36 @@
+﻿
+using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebRunner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VideoCapture capture = new VideoCapture(1); //create a camera capture
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap image = capture.QueryFrame().Bitmap; //take a picture
+            //image.Save("test.png");
+            pictureBox1.Image = image;
+        }
+    }
+}
